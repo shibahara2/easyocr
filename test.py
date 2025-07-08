9cdae3bad4a4e4469a9bc17de1fcabc06486a5a5
Naoki Shibahara <shibahara@A1016620.local>
2025-07-08 11:40:07 +0900

    feat: flask server

--- test.py
+++ test.py
@@ -1,6 +1,39 @@
 import easyocr
+from flask import Flask, request, jsonify, Response
+from pdf2image import convert_from_bytes
+from PIL import Image
+import json
 
-reader = easyocr.Reader(['en'])
-result = reader.readtext('sample_contract.png', detail=0)
+app = Flask(__name__)
 
-print(result)
+@app.route('/')
+def index():
+    return 'Welcome to EasyOCR server.'
+
+@app.route('/ocr', methods=['POST'])
+def ocr():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file uploaded'}), 400
+    
+    uploaded_file = request.files['file']
+    filename = uploaded_file.filename.lower()
+
+    if filename.endswith('.pdf'):
+        images = convert_from_bytes(uploaded_file.read())
+    elif filename.endswith('.png') or filename.endswith('.jpg'):
+        image = Image.open(uploaded_file.stream)
+        images = [image]
+    else:
+        return jsonify({'error': 'Unsupported file type'}), 415
+
+    results = {}
+    reader = easyocr.Reader(['en'])
+    for i, image in enumerate(images):
+        text = reader.readtext(image, detail = 0)
+        results['page-' + str(i+1)] = text
+
+    return Response(json.dumps(results, ensure_ascii=False), mimetype='application/json')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5555)
+    
